c6ae733cbeacb7b0566e86e9c7b4b57b0e73edeb
Anette Aguraiuja <anetteagura@gmail.com>
2020-05-31 16:22:41 +0300

    Parandatud examples.py

--- example.py
+++ example.py
@@ -21,39 +21,31 @@ def albumiteKriipsud():
 
 
 print("Tere, Paavo!")
-mitmesDemo = int(input("Mitmendat demo soovid näha? 1/2 "))
+mitmesDemo = int(input("Mitmendat demo soovid näha? 1/2/3: "))
 if mitmesDemo == 1:
     b = "jah"
     print("Kas soovid lisada vinüülplaatide nimekirja albumit? jah/ei \033[96mjah\033[0m")
-    input("Vajutage sisestusklahvi, et demos edasi liikuda.")
 
 
     if b == "jah":
         seis = "aktiivne"
         grupp = "Anne Veski"
         print("Sisestage esitaja nimi: \033[96mAnne Veski\033[0m")
-        input("Vajutage sisestusklahvi, et demos edasi liikuda.")
         album = "Eesti kullafond"
         print("Sisestage albumi nimi: \033[96mEesti kullafond\033[0m")
-        input("Vajutage sisestusklahvi, et demos edasi liikuda.")
         aasta = "2006"
         print("Sisestage aasta: \033[96m2006\033[0m")
-        input("Vajutage sisestusklahvi, et demos edasi liikuda.")
 
         while seis == "aktiivne":
         
             laul = "Roosiaia kuninganna"
             print("Sisestage laulu pealkiri: \033[96mRoosiaia kuninganna\033[0m")
-            input("Vajutage sisestusklahvi, et demos edasi liikuda.")
             laulupealkiri = grupp.title() + "\t" + album.title() + "\t" + aasta + "\t" + laul.title()
             fail = open("albumid.txt", "a", encoding="UTF-8")
             fail.write("\n" + laulupealkiri)
-        
             print("Kas soovite veel laule lisada? jah/ei \033[96mjah\033[0m")
-            input("Vajutage sisestusklahvi, et demos edasi liikuda.")
             laul = "Jätke võtmed väljapoole"
             print("Sisestage laulu pealkiri: \033[96mJätke võtmed väljapoole\033[0m")
-            input("Vajutage sisestusklahvi, et demos edasi liikuda.")
             laulupealkiri = grupp.title() + "\t" + album.title() + "\t" + aasta + "\t" + laul.title()
             fail = open("albumid.txt", "a", encoding="UTF-8")
             fail.write("\n" + laulupealkiri)
@@ -65,57 +57,60 @@ if mitmesDemo == 1:
 
     y = "ei"
     print("Kas soovid näha vinüülplaatide nimekirja? jah/ei \033[96mei\033[0m")
-
-    input("Vajutage sisestusklahvi, et demos edasi liikuda.")
-
     fail = open("albumid.txt", encoding="UTF-8")
     albumid = []
 
 
     if y == "jah":
-        
         albumiteKriipsud()
     
     else:
         w = "jah"
         print("Kas soovid otsida laulu? jah/ei \033[96mjah\033[0m")
-        input("Vajutage sisestusklahvi, et demos edasi liikuda.")
-
         if w == "jah":
-
             for rida in fail:
                 albumid.append(rida)
     
-
             nimi = "Alice Cooper"
             print("Sisesta albumi või artisti nimi: \033[96mAlice Cooper\033[0m")
-            input("Vajutage sisestusklahvi, et demos edasi liikuda.")
             x = nimi.title()
 
             for str in albumid:
                 if x in str:
-                    print(str)
-                
+                    print(str)  
         else:
             print("Ei soovinud midagi!")
-
     fail.close()
 
-else:
-    print("Kas soovid lisada vinüülplaatide nimekirja albumit? jah/ei \033[96mjah\033[0m")
-    input("Vajutage sisestusklahvi, et demos edasi liikuda.")    
-
-
-    y = "jah"
-    print("Kas soovid näha vinüülplaatide nimekirja? jah/ei \033[96mei\033[0m")
-
-    input("Vajutage sisestusklahvi, et demos edasi liikuda.")
+elif mitmesDemo == 2:
+    print("Kas soovid lisada vinüülplaatide nimekirja albumit? jah/ei \033[96mei\033[0m")
+    print("Kas soovid näha vinüülplaatide nimekirja? jah/ei \033[96mjah\033[0m")
 
     fail = open("albumid.txt", encoding="UTF-8")
     albumid = []
+    y = "jah"
 
     if y == "jah":
-        
         albumiteKriipsud()
         
+    fail.close()
+
+elif mitmesDemo == 3:
+    print("Kas soovid lisada vinüülplaatide nimekirja albumit? jah/ei \033[96mei\033[0m")
+    print("Kas soovid näha vinüülplaatide nimekirja? jah/ei \033[96mei\033[0m")
+    w = "jah"
+    print("Kas soovid otsida laulu? jah/ei \033[96mjah\033[0m")
+    if w == "jah":
+        albumid = []
+        fail = open("albumid.txt", encoding="UTF-8")
+        for rida in fail:
+            albumid.append(rida)
+    
+        nimi = "Aerosmith"
+        print("Sisesta albumi või artisti nimi: \033[96mAerosmith\033[0m")
+        x = nimi.title()
+
+        for str in albumid:
+            if x in str:
+                print(str)
     fail.close()
